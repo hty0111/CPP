fdba5dc37b7bf7d176333e395d6a794a7f654060
HTY <1044213317@qq.com>
2021-08-17 19:01:24 +0800

    dev

--- C++ Primer Plus/chap12/note.cpp
+++ C++ Primer Plus/chap12/note.cpp
@@ -4,11 +4,16 @@
 
 #include <iostream>
 
-class Class;
+class Class{
+    const int INT;
+    Class(const Class &);       //复制构造函数的声明，默认的复制构造函数逐个复制非静态成员
+    Class(int con) : INT(con);  //构造函数可以用初始化列表语法，非静态const和引用必须使用这种语法
+};
+Class::Class(int con) : INT(con){}     //给常量赋值
 
 int main()
 {
     Class a;
     Class b = a; // <=> Class b = Class(a);
-    Class(const Class &);   //复制构造函数的声明，默认的复制构造函数逐个复制非静态成员
+
 }
